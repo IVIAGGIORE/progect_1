7915d57891a2b6b3a25c9bc7621e2607250cc244
IVIAGGIORE <86253502+IVIAGGIORE@users.noreply.github.com>
2021-07-05 22:13:46 +0300

    Add files via upload

---
+++ src/com/training/exproject/package1/Data.java
@@ -0,0 +1,39 @@
+package com.training.exproject.package1;
+
+public class Data {
+	private String surname;
+	private String initials;
+	private int numberGroup;
+	private int progress;
+	
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getSurnume() {
+		return this.surname;
+	}
+	
+	public void setInitials(String initials) {
+		this.initials = initials;
+	}
+	public String getInitialis() {
+		return this.initials;
+	}
+	
+	public void setNumberGroup(int numberGroup) {
+		this.numberGroup = numberGroup;
+	}
+	public int getNumberGroup() {
+		return this.numberGroup;
+	}
+	
+	public void setProgress(int progress) {
+		this.progress = progress;
+	}
+	
+	public int getProgress() {
+		return this.progress;
+	}
+	
+	
+}
